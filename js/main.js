cd4fb26e62b6dac147ccca8e463fafb932b130c0
sirhmd0 <sirh00@outlook.com>
2020-04-23 16:40:24 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+    // Navigation Bar toggler
+
+    $(".nav-btn").click(function () {
+        $("nav").toggleClass("nav-toggle");
+        $(".nav-menu").toggleClass("nav-menu-toggle");
+
+    });
+
+    $(".nav-menu a").click(function () {
+        $("nav").removeClass("nav-toggle");
+        $(".nav-menu").removeClass("nav-menu-toggle");
+
+    });
+
+
+    // Button for Srolling in Top
+
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop()) {
+            $(".btnScroll").css("display", "flex");
+        } else {
+            $(".btnScroll").css("display", "none");
+
+        }
+    });
+
+    $(window).scroll(function () {
+        $(".btnScroll").css("visibility", "visible");
+    });
+});
